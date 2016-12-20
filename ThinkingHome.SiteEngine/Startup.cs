40dd23534a7ba121d6c20d51824aac04291e52cd
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-12-20 13:11:10 +0300

    index page

--- ThinkingHome.SiteEngine/Startup.cs
+++ ThinkingHome.SiteEngine/Startup.cs
@@ -49,7 +49,7 @@ namespace ThinkingHome.SiteEngine
             routes.MapRoute(
                 name: "home",
                 template: string.Empty,
-                defaults: new { controller = "Home", action = "Md" });
+                defaults: new { controller = "Home", action = "Index" });
 
             routes.MapRoute(
                 name: "md",
